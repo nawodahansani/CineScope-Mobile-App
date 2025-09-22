a3ff224350708518d79e726d8f95a4f2461e1698
Nawoda Hansani <nawodahansani13@gmail.com>
2025-09-22 14:51:22 +0530

    checked project execution

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import "./../global.css"
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
