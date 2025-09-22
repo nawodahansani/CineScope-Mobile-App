f9cf75b6618601d50b540e813f77e87b0c684225
Nawoda Hansani <nawodahansani13@gmail.com>
2025-09-22 17:24:30 +0530

    update rootlayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,15 @@
 import "./../global.css"
 import { View, Text, Pressable } from 'react-native'
 import React from 'react'
-import { Link, useRouter } from "expo-router"
+import { Link, Slot, useRouter } from "expo-router"
+import { AuthProvider } from "@/context/AuthContext"
 
 const RootLayout = () => {
     const router = useRouter()
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <AuthProvider>
+      <Slot />
+    </AuthProvider>
   )
 }
 
