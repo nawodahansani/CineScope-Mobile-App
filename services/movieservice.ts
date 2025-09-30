0cb67d709e46420e1447f1d5187a87cc040ca100
Nawoda Hansani <nawodahansani13@gmail.com>
2025-09-30 19:10:47 +0530

    update movieservice.ts

---
+++ services/movieservice.ts
@@ -0,0 +1,25 @@
+import { TMDB_API_KEY } from "@env";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const fetchPopularMovies = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`);
+    const data = await response.json();
+    return data.results; // array of movies
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    return [];
+  }
+};
+
+export const fetchMovieDetails = async (movieId: number) => {
+  try {
+    const response = await fetch(`${BASE_URL}/movie/${movieId}?api_key=${TMDB_API_KEY}&language=en-US`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    return null;
+  }
+};
