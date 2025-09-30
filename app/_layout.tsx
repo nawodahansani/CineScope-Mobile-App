be27a2642b06fbced33ceff36cb11d99b05e19ba
Nawoda Hansani <nawodahansani13@gmail.com>
2025-09-30 21:25:52 +0530

    done changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { Slot, useRouter } from "expo-router"
 import { AuthProvider } from "@/context/AuthContext"
 
 const RootLayout = () => {
-    const router = useRouter()
   return (
     <AuthProvider>
       <Slot />
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 import React, { useEffect } from 'react'
 import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'expo-router';
