7416483e4e39340eea15f21b34ae48488b217d53
Nawoda Hansani <nawodahansani13@gmail.com>
2025-09-30 18:23:50 +0530

    update  dashboard-_layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,7 +4,12 @@ import { Stack } from 'expo-router'
 
 const AuthLayout = () => {
   return (
-    <Stack>
+    <Stack
+    screenOptions={{
+        headerShown: false,
+        animation: 'slide_from_right',
+      }}
+      >
         <Stack.Screen name="login" options={{title: "Login"}} />
         <Stack.Screen name="register" options={{title: "Register"}} />
     </Stack>
--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'; 
+
+
+const tabs: { label: string; name: string; icon: "home-outline" | "list-outline" | "person-outline" | "settings-outline" }[] = [
+    {label: "Home", name: "home", icon: "home-outline"},
+    {label: "Tasks", name: "tasks", icon: "list-outline"},
+    {label: "Profile", name: "profile", icon: "person-outline"},
+    {label: "Setting", name: "setting", icon: "settings-outline"}
+]
 
 const DashboardLayout = () => {
   return (
-    <View>
-      <Text>DashboardLayout</Text>
-    </View>
+    <Tabs>
+        {tabs.map(({ label, name, icon }) => (
+            <Tabs.Screen 
+            key={name}
+            name={name}
+            options={{
+                title: label,
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name={icon} size={size} color={color} />
+                )
+            }}
+        />
+        ))}
+    </Tabs>
   )
 }
 
